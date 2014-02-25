1a79e38d99c8b12dc8e342669dd1f0e0b5c217fc
Ilya Pashchenko <in4pashchenko@gmail.com>
2014-02-25 18:54:53 +0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import uvmod
+try:
+    from setuptools import setup
+    setup
+except ImportError:
+    from distutils.core import setup
+    setup
+
+
+setup(
+    name='uvmod',
+    version=uvmod.version,
+    author='Ilya Pashchenko',
+    author_email='in4pashchenko@gmail.com',
+    packages=['uvmod'],
+    scripts=[],
+    url='',
+    test_suit='tests'
+    license='LICENSE',
+    description='Simple models',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "numpy >= 1.7.0",
+        "scipy == 0.13.3",
+    ],
