8ed8eb735549aa691a92c98a84f9c5cccb20faf8
Ilya Pashchenko <in4pashchenko@gmail.com>
2014-09-24 17:49:20 +0400

    Now use LSQ for 2d fitting for all baselines

--- uvmod/ask_source.py
+++ uvmod/ask_source.py
@@ -2,14 +2,15 @@ import os
 import sys
 path = os.path.normpath(os.path.join(os.path.dirname(sys.argv[0]), '..'))
 sys.path.insert(0, path)
-from uvmod import utils
+from uvmod import utils, models, ra_uvfit
 import math
 import psycopg2
 import numpy as np
 import argparse
-from plotting import scatter_3d_errorbars
+from plotting import scatter_3d_errorbars, plot_all
 
 # TODO: Plot RA baselines with different colors!
+# TODO: Use only range of baselines for fitting! Otherwise it is useless...
 
 dtype_converter_dict = {'integer': 'int', 'smallint': 'int', 'character': '|S',
                         'character varying': '|S', 'real': '<f8',
@@ -132,6 +133,17 @@ if __name__ == '__main__':
     parser.add_argument('-band', action='store', nargs='?',
                         default=None, metavar='band',
                         type=str, help='- k, c, l or p')
+    parser.add_argument('-leastsq', action='store_true', dest='use_leastsq',
+                        default=False,
+                        help='- use scipy.optimize.leastsq for analysis of'
+                             ' detections')
+    parser.add_argument('-p0', action='store', dest='p0', nargs='+',
+                        default=None, type=float, help='- starting estimates'
+                                                       ' for the minimization'
+                                                       ' or center of initial'
+                                                       ' ball for MCMC')
+    parser.add_argument('-plot_model', action='store_true', dest='plot_model',
+                        default=False, help='plot fitted model?')
 
     args = parser.parse_args()
 
@@ -153,10 +165,10 @@ if __name__ == '__main__':
     struct_array = get_source_array_from_dbtable(source, band)
     print struct_array
     # Put u,v from lambda to E.D
-    struct_array['u'] = utils.uv_to_ed(struct_array['u'],
-                                       lambda_cm=utils.band_cm_dict[band])
-    struct_array['v'] = utils.uv_to_ed(struct_array['v'],
-                                       lambda_cm=utils.band_cm_dict[band])
+    #struct_array['u'] = utils.uv_to_ed(struct_array['u'],
+    #                                   lambda_cm=utils.band_cm_dict[band])
+    #struct_array['v'] = utils.uv_to_ed(struct_array['v'],
+    #                                   lambda_cm=utils.band_cm_dict[band])
 
     if args.ra_only:
         struct_array = struct_array[np.where(np.logical_or(struct_array['st1']
@@ -200,21 +212,31 @@ if __name__ == '__main__':
             x1, x2, y, sy = zip(detections)
         except ValueError:
             detections_ = np.atleast_2d(detections)
-            x1 = detections_[:, 0]
-            x2 = detections_[:, 1]
+            x1 = utils.uv_to_ed(detections_[:, 0],
+                                lambda_cm=utils.band_cm_dict[band])
+            x2 = utils.uv_to_ed(detections_[:, 1],
+                                lambda_cm=utils.band_cm_dict[band])
+            # Keep in uv not ED!
+            xx = detections_[:, :2]
             y = detections_[:, 2]
             sy = detections_[:, 3]
     else:
+        # We need them as ``None`` to put all data in one plotting function.
         x1, x2, y, sy = [None] * 4
     if ulimits:
         try:
             ux1, ux2, uy = zip(ulimits)
         except ValueError:
             ulimits_ = np.atleast_2d(ulimits)
-            ux1 = ulimits_[:, 0]
-            ux2 = ulimits_[:, 1]
+            ux1 = utils.uv_to_ed(ulimits_[:, 0],
+                                 lambda_cm=utils.band_cm_dict[band])
+            ux2 = utils.uv_to_ed(ulimits_[:, 1],
+                                 lambda_cm=utils.band_cm_dict[band])
+            # Keep in uv not ED!
+            uxx = ulimits_[:, :2]
             uy = ulimits_[:, 2]
     else:
+        # We need them as ``None`` to put all data in one plotting function.
         ux1, ux2, uy = [None] * 3
 
     if args.save_to_file:
@@ -229,3 +251,42 @@ if __name__ == '__main__':
     scatter_3d_errorbars(x1=x1, x2=x2, y=y, sy=sy, ux1=ux1, ux2=ux2, uy=uy,
                          savefig=savefig)
 
+    # Now fitting detections and upper limits
+    if args.use_leastsq:
+        if not detections:
+            raise Exception("Need detections for LSQ!")
+        p0 = args.p0
+        if len(p0) == 2:
+            print "Fitting isotropic 2d-gaussian model to data!"
+            model_to_use = models.Model_2d_isotropic
+        elif len(p0) == 4:
+            print "Fitting anisotropic 2d-gaussian model to data!"
+            model_to_use = models.Model_2d_anisotropic
+        else:
+            raise Exception("Work only with single 2d gaussian model (2 or 4"
+                            "pars)!")
+        # Put mas in args.p0 to uv for major axis.
+        p0[1] = 1./ (2. * math.pi * p0[1] * utils.mas_to_rad)
+        print "xx : ", xx
+        print "y : ", y
+        print "p0 after conv to uv spatial frequencies : ", p0
+        lsq = ra_uvfit.LS_estimates(xx, y, model_to_use, sy=sy)
+        p, pcov = lsq.fit(p0)
+        print p, pcov
+        # Put from uv to mas for major axis.
+        p_mas = p.copy()
+        p_mas[1] = 1. / (2. * math.pi * p[1] * utils.mas_to_rad)
+        try:
+            pcov = np.atleast_2d(pcov)
+        except:
+            print "Sucked up with covariance estimation!"
+        pcov[1, :] = 1. / (2. * math.pi * pcov[1, :] * utils.mas_to_rad)
+        pcov[:, 1] = 1. / (2. * math.pi * pcov[:, 1] * utils.mas_to_rad)
+
+        print "LSQ gave p: ", p_mas
+        print "LSQ gave pcov: ", pcov
+
+        if args.plot_model:
+            p[1] = utils.uv_to_ed(p[1], lambda_cm=utils.band_cm_dict[band])
+            print "Plotting model with parameters : ", p
+            plot_all(p, x1, x2, y, sy, n=100)
