f042791b6c3041fbb37494293f85f418ecf9ba2b
Ilya Pashchenko <in4pashchenko@gmail.com>
2015-01-28 01:58:53 +0300

    1d ok

--- bin/fit_amp.py
+++ bin/fit_amp.py
@@ -12,8 +12,9 @@ sys.path.insert(0, path)
 from uvmod import stats
 from uvmod import models
 from uvmod import plotting
+import matplotlib.pyplot as plt
 try:
-    from pylab import (errorbar, plot, savefig)
+    from pylab import (errorbar, plot, savefig, show)
     is_pylab = True
 except ImportError:
     is_pylab = False
@@ -103,15 +104,11 @@ if __name__ == '__main__':
         sys.exit("Use -max_p flag to specify the list of maximum values of"
                  " parameters in uniform prior distributions")
     if (not args.use_leastsq) and args.p0 and (not args.std0):
-        sys.exit("Use -std0 flag to specify value of std for initial parametes"
-                 " ball")
+        print("Initializing MCMC using default radius around p0!")
     if (not args.use_leastsq) and args.std0 and (not args.p0):
-        sys.exit("Use -p0 flag to specify the center of ball for initial"
-                 " parameters values")
+        print("Initializing MCMC in (0, 1) interval!")
     if (not args.use_leastsq) and (not args.p0) and (not args.std0):
-        print("Use -p0 flag to specify the center of ball for initial"
-              " parameters values!")
-        print("Use -std0 flag to specify value of std for that ball!")
+        print("Initializing MCMC in (0, 1) interval!")
 
     print(parser.parse_args())
 
@@ -124,6 +121,7 @@ if __name__ == '__main__':
     # TODO: Refactor to function func(fname, tuple_of_dim, optional_tuple)
     if not args.use_2d:
         model = models.Model_1d
+        n_gauss_pars = 2
         try:
             x, y, sy = np.loadtxt(args.path_to_detections, unpack=True)
         except ValueError:
@@ -180,7 +178,7 @@ if __name__ == '__main__':
     if args.path_to_ulimits is not None:
         print("Predictors of upper limits: ")
         print(xl)
-        print("Upper limits: ")
+        print("Upper limits values: ")
         print(yl)
         if syl is not None:
             print("Upper limits uncertainties: ")
@@ -235,10 +233,14 @@ if __name__ == '__main__':
         # Using affine-invariant MCMC
         nwalkers = 250
         ndim = len(lnprs)
-        if not args.p0:
+        if args.p0 is None:
             p0 = np.random.uniform(low=0., high=1., size=(nwalkers, ndim))
         else:
-            p0 = emcee.utils.sample_ball(args.p0, args.std0, size=nwalkers)
+            if args.std0 is None:
+                std0 = args.std0
+            else:
+                std0 = [0.01] * len(args.p0)
+            p0 = emcee.utils.sample_ball(args.p0, std0, size=nwalkers)
         sampler = emcee.EnsembleSampler(nwalkers, ndim, lnpost)
         print("Burning-in...")
         pos, prob, state = sampler.run_mcmc(p0, 150)
@@ -257,7 +259,7 @@ if __name__ == '__main__':
 
         # Save mean and 95% HDI interval of parameter's posterior
         if args.savefile:
-            print ("Saving data to " + args.savefile)
+            print ("Saving mean & 95%-HDI to " + args.savefile)
             np.savetxt(args.savefile, np.asarray(par_list))
 
         # Visualize with triangle_plot.py
@@ -266,7 +268,7 @@ if __name__ == '__main__':
             # parameters posterior
             if is_triangle:
                 figure = triangle.corner(sampler.flatchain[::10, :])
-                print ("Saving figure to " + args.savefig)
+                print ("Saving parameter's posterior plot to " + args.savefig)
                 figure.savefig(args.savefig)
             # Plot histogram stuff wo triangle
             else:
@@ -276,16 +278,19 @@ if __name__ == '__main__':
         if args.savemodfig:
             # 2D case
             if args.use_2d:
-                print(par_array[:n_gauss_pars], x1, x2, y, sy, xl1, xl2, yl)
+                print ("Saving model vs. data plot to " + args.savemodfig)
                 plotting.plot_all(par_array[:n_gauss_pars, 1], x1=x1, x2=x2, y=y,
                                   sy=sy, ux1=xl1, ux2=xl2, uy=yl,
                                   outfile=args.savemodfig)
             # 1D case
             else:
-                errorbar(x, y, sy, fmt='.k')
-                errorbar(xl, yl, syl, fmt='.r', lolims=True)
+                fig = plt.figure()
+                ax = fig.add_subplot(111)
+                ax.errorbar(x, y, sy, fmt='.k')
+                ax.errorbar(xl, yl, syl, fmt='.r', lolims=True)
                 model_plot = model(np.arange(1000.) * xmax / 1000.)
-                plot(np.arange(1000.) * xmax / 1000.,
-                     model_plot(par_array[:n_gauss_pars, 1]))
-                print ("Saving figure to " + args.savemodfig)
-                savefig(args.savemodfig)
+                ax.plot(np.arange(1000.) * xmax / 1000.,
+                        model_plot(par_array[:n_gauss_pars, 1]))
+                fig.show()
+                print ("Saving model vs. data plot to " + args.savemodfig)
+                fig.savefig(args.savemodfig)
